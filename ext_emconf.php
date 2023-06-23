d1c72cf059786f4ed57faf00b5f51dddf23757fb
Samuel Mihal <samuel.mihal@devsk.io>
2023-06-23 11:42:25 +0200

    [TASK] Support Typo3 v12

---
+++ ext_emconf.php
@@ -0,0 +1,9 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Content Element Registry CLI',
+    'description' => '',
+    'category' => 'be',
+    'state' => 'stable',
+    'author' => 'DEVSK'
+];
